9abfdcbc7ccb8f2475d44b1db24eb0601fc4cb2f
Creator <email.com>
2024-11-22 21:57:59 +0700

    t

--- lab3/sgemv.cpp
+++ lab3/sgemv.cpp
@@ -1,6 +1,8 @@
-#include <mpi.h>
 #include <stdio.h>
+#include <mpi.h>
 #include <stdlib.h>
+#include <inttypes.h>
+#include <time.h>
 
 void *xmalloc(size_t size) 
 {
@@ -12,24 +14,24 @@ void *xmalloc(size_t size)
     }
     return ptr;
 }
-
+// O(mn / p + n + m)
 void get_chunk(int a, int b, int commsize, int rank, int *lb, int *ub)
 {
     /* OpenMP 4.0 spec (Sec. 2.7.1, default schedule for loops)
-     * For a team of commsize processes and a sequence of n items, let ceil(n ? commsize) be the integer q
-     * that satisfies n = commsize * q - r, with 0 <= r < commsize.
-     * Assign q iterations to the first commsize - r procs, and q - 1 iterations to the remaining r processes */
+    * For a team of commsize processes and a sequence of n items, let ceil(n ? commsize) be the integer q
+    * that satisfies n = commsize * q - r, with 0 <= r < commsize.
+    * Assign q iterations to the first commsize - r procs, and q - 1 iterations to the remaining r processes */
     int n = b - a + 1;
     int q = n / commsize;
-    if (n % commsize)
+    if (n % commsize) 
         q++;
     int r = commsize * q - n;
     /* Compute chunk size for the process */
     int chunk = q;
-    if (rank >= commsize - r)
+    if (rank >= commsize - r) 
         chunk = q - 1;
     *lb = a; /* Determine start item for the process */
-    if (rank > 0)
+    if (rank > 0) 
     { /* Count sum of previous chunks */
         if (rank <= commsize - r)
             *lb += q * rank;
@@ -39,82 +41,97 @@ void get_chunk(int a, int b, int commsize, int rank, int *lb, int *ub)
     *ub = *lb + chunk - 1;
 }
 
-/* dgemv: Compute matrix-vector product c[m] = a[m][n] * b[n] */
-void dgemv(double *a, double *b, double *c, int m, int n)
+void dgemv(float *a, float *b, float *c, int m, int n)
 {
     int commsize, rank;
     MPI_Comm_size(MPI_COMM_WORLD, &commsize);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
     int lb, ub;
     get_chunk(0, m - 1, commsize, rank, &lb, &ub);
     int nrows = ub - lb + 1;
-    for (int i = 0; i < nrows; i++)
+
+    for (int i = 0; i < nrows; i++) 
     {
         c[lb + i] = 0.0;
         for (int j = 0; j < n; j++)
             c[lb + i] += a[i * n + j] * b[j];
     }
-    // Gather data: each process contains sub-result in c[m] in rows [lb..ub]
-    if (rank == 0)
+
+    int *displs = (int *)malloc(sizeof(*displs) * commsize);
+    if (displs == NULL) 
     {
-        int *displs = malloc(sizeof(*displs) * commsize);
-        int *rcounts = malloc(sizeof(*rcounts) * commsize);
-        for (int i = 0; i < commsize; i++)
-        {
-            int l, u;
-            get_chunk(0, m - 1, commsize, i, &l, &u);
-            rcounts[i] = u - l + 1;
-            displs[i] = (i > 0) ? displs[i - 1] + rcounts[i - 1] : 0;
-        }
-        MPI_Gatherv(MPI_IN_PLACE, ub - lb + 1, MPI_DOUBLE, c, rcounts, displs, MPI_DOUBLE, 0,
-                    MPI_COMM_WORLD);
+        fprintf(stderr, "Error: Memory allocation failed for displs\n");
+        MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
     }
-    else
+
+    int *rcounts = (int *)malloc(sizeof(*rcounts) * commsize);
+    if (rcounts == NULL) 
     {
-        MPI_Gatherv(&c[lb], ub - lb + 1, MPI_DOUBLE, NULL, NULL, NULL, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        fprintf(stderr, "Error: Memory allocation failed for rcounts\n");
+        MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
     }
+
+    for (int i = 0; i < commsize; i++) 
+    {
+        int l, u;
+        get_chunk(0, m - 1, commsize, i, &l, &u);
+        rcounts[i] = u - l + 1;                     // Количество элементов от каждого процесса
+        displs[i] = (i > 0) ? displs[i - 1] + rcounts[i - 1] : 0; // Смещений для каждой части
+    }
+
+    // Сбор всех частей вектора C
+    MPI_Allgatherv(&c[lb], nrows, MPI_FLOAT, c, rcounts, displs, MPI_FLOAT, MPI_COMM_WORLD);
+
+    // Освобождаем память
+    free(displs);
+    free(rcounts);
 }
+
 
 int main(int argc, char **argv)
 {
     int commsize, rank;
+    int n = 45000;
+    int m = 45000;
+    double start_time;
+    double end_time;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &commsize);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    double t = wtime();
+
+    //double t = wtime();
+    if (rank == 0)
+        start_time = MPI_Wtime();
     int lb, ub;
     get_chunk(0, m - 1, commsize, rank, &lb, &ub); // Декомпозиция матрицы на горизонтальные полосы
     int nrows = ub - lb + 1;
-    double *a = xmalloc(sizeof(*a) * nrows * n);
-    double *b = xmalloc(sizeof(*b) * n);
-    double *c = xmalloc(sizeof(*c) * m);
+    float *a = (float *)xmalloc(sizeof(*a) * nrows * n);
+    float *b = (float *)xmalloc(sizeof(*b) * n);
+    float *c = (float *)xmalloc(sizeof(*c) * m);
     // Each process initialize their arrays
-    for (int i = 0; i < nrows; i++)
+    for (int i = 0; i < nrows; i++) 
     {
         for (int j = 0; j < n; j++)
             a[i * n + j] = lb + i + 1;
     }
     for (int j = 0; j < n; j++)
         b[j] = j + 1;
+
     dgemv(a, b, c, m, n);
-    t = wtime() - t;
+    MPI_Barrier(MPI_COMM_WORLD);
     if (rank == 0)
+        end_time = MPI_Wtime();
+    //t = wtime() - t;
+   
+    if (rank == 0) 
     {
-        // Validation
-        for (int i = 0; i < m; i++)
-        {
-            double r = (i + 1) * (n / 2.0 + pow(n, 2) / 2.0);
-            if (fabs(c[i] - r) > 1E-6)
-            {
-                fprintf(stderr, "Validation failed: elem %d = %f (real value %f)\n", i, c[i], r);
-                break;
-            }
-        }
+        printf("Count proc: %d\n", commsize);
         printf("DGEMV: matrix-vector product (c[m] = a[m, n] * b[n]; m = %d, n = %d)\n", m, n);
-        printf("Memory used: %" PRIu64 " MiB\n", (uint64_t)(((double)m * n + m + n) * sizeof(double)) >> 20);
-        double gflop = 2.0 * m * n * 1E-9;
-        printf("Elapsed time (%d procs): %.6f sec.\n", commsize, t);
-        printf("Performance: %.2f GFLOPS\n", gflop / t);
+        printf("Proc memory used: %" PRIu64 " MiB\n", (uint64_t)((float)((m * n) / commsize + m + n) * sizeof(float)) >> 20);
+        printf("Memory used: %" PRIu64 " MiB\n", (uint64_t)((float)(((m * n) / commsize + m + n)* commsize) * sizeof(float)) >> 20);
+        printf("Elapsed time (%d procs): %lf sec.\n", commsize, end_time - start_time);
+        
     }
     free(a);
     free(b);
